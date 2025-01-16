9991d9616b96a93af1486de5c2a8d4947b846f3e
Strroke21 <rishipatil1707@gmail.com>
2025-01-16 11:28:55 +0530

    new file

---
+++ install_requirements.sh
@@ -0,0 +1,10 @@
+
+
+#install dependencies
+
+pip3 install dronekit
+pip3 install pymavlink
+sudo apt install python3-opencv
+pip3 install argparse
+pip3 install numpy
+sudo pip3 install mavproxy #running as a root user for elevated permissions
