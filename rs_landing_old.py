35b9e7d65af730f546b38da170742fc8898fe4df
Strroke21 <rishipatil1707@gmail.com>
2026-04-13 20:18:32 +0530

    angle conversion

--- rs_landing_old.py
+++ rs_landing_old.py
@@ -14,7 +14,7 @@ flatness, final_alt = 0.2, 4
 disparity_to_depth_scale = 0.0010000000474974513
 MAX_DISTANCE = 20.0
 
-fcu_addr = 'udp:127.0.0.1:14560' 
+fcu_addr = 'udp:127.0.0.1:14561' 
 current_target = None
 
 HYST_THRESHOLD = 5   # frames required to switch
